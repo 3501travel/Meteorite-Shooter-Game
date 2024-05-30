a6248746e873f1479b3d8594dcf22640dd4c1dfc
Erim Satar <erim.satar@valensas.com>
2024-05-30 20:46:53 +0300

    scoring added

---
+++ My project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreTextTMP;
+
+    private int score = 0;
+    public int Score => score;
+
+    private int live = 5;
+    public int Live => live;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreTextTMP = FindObjectOfType<TextMeshProUGUI>();
+        scoreTextTMP.text = "Score: " + score.ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void increaseScore(int amount)
+    {
+        score += amount;
+        scoreTextTMP.text = "Score: " + score.ToString();
+
+        // You can add UI update logic here if you have a UI element to display the score
+    }
+
+    public void changeLive(int amount)
+    {
+        live += amount;
+    } 
+}
